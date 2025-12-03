20bc09cf5650a366ae8f58fccfc5752169f40b67
Bouiyodajoseph <bouiyodajoseph@gmail.com>
2025-12-03 23:13:26 +0100

    deployer2

--- src/modules/redis/redis.module.ts
+++ src/modules/redis/redis.module.ts
@@ -1,14 +1,14 @@
 import { Module, Global } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config'; // <-- NOUVEL IMPORT
+import { ConfigModule } from '@nestjs/config';
 import { redisProvider } from './redis.provider';
 
 @Global()
 @Module({
-  // =======================================================
-  // ==                 CORRECTION ICI                    ==
-  // =======================================================
-  // On doit importer ConfigModule pour que les providers de ce module
-  // (ici, notre redisProvider) puissent injecter ConfigService.
+  /**
+   * On importe ConfigModule ici pour garantir que ConfigService
+   * sera disponible à l'injection pour les providers de CE module
+   * (notamment notre redisProvider).
+   */
   imports: [ConfigModule],
   providers: [redisProvider],
   exports: [redisProvider],
